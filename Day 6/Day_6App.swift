964407111a38c8d87422c74649b787bf0c0a6e10
shriya <shriya.dharmendran@gmail/com>
2023-06-12 11:23:30 -0400

    Initial Commit

---
+++ Day 6/Day_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_6App.swift
+//  Day 6
+//
+//  Created by Shriya Dharmendran on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Day_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
